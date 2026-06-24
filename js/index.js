ceb608da741f21cc0642e79868ad1234a58251b8
Hideki Yagi <yagihideki6@gmail.com>
2026-06-24 18:01:57 -0300

    feat: adiciona a busca pelo o usuario ao clicar no botao enter

--- js/index.js
+++ js/index.js
@@ -5,22 +5,21 @@ const inputSearch = document.getElementById('input-search');
 const btnSearch = document.getElementById('btn-search');
 const profileResults = document.querySelector('.profile-results');
 
-btnSearch.addEventListener('click', async () => {
-    const userName = inputSearch.value;
-    if (!userName) {
-        alert('Por favor, digite um nome de usuário do GitHub.');
-        profileResults.innerHTML = "";
-        return;
-    }
-    profileResults.innerHTML = `<p class="loading">Carregando...</p>`;
-    try {
-        const userData = await fetchGithubUser(userName);
-        const userRepos = await fetchGithubUserRepos(userName);
-        console.log(userRepos);
-        renderProfile(userData, userRepos, profileResults);
-    } catch (error) {
-        console.error('Erro ao buscar o perfil do usuário:', error);
-        alert('Usuário não encontrado. Por favor, verifique o nome de usuário e tente novamente.');
-        profileResults.innerHTML = "";
-    }
-});
+const searchGithubUser = async () => {
+  const userName = inputSearch.value.trim();
+  if (!userName) return alert('Digite um nome de usuário do GitHub.');
+
+  profileResults.innerHTML = '<p class="loading">Carregando...</p>';
+
+  try {
+    const userData = await fetchGithubUser(userName);
+    const userRepos = await fetchGithubUserRepos(userName);
+    renderProfile(userData, userRepos, profileResults);
+  } catch {
+    alert('Usuário não encontrado.');
+    profileResults.innerHTML = '';
+  }
+};
+
+btnSearch.addEventListener('click', searchGithubUser);
+inputSearch.addEventListener('keydown', (event) => event.key === 'Enter' && searchGithubUser());
